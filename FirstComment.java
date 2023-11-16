a025ef48594b1bee93481f8c7c3f261b0373bcb0
nanamuramarina <nanamuramarina@gmail.com>
2023-11-16 14:14:25 +0000

    プログラミングの構造

---
+++ FirstComment.java
@@ -0,0 +1,9 @@
+/* FirstComment.java
+   文字列を表示するプログラム
+   2021.9.16 菅原作成 */
+
+class FirstComment{
+  public static void main (String[] args) {
+    System.out.println("Javaを楽しもう！");  //画面表示
+  }
+}
